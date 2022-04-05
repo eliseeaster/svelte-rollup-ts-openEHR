915ca1ecbb0134c991abe7f8f40e6e88e96ed864
Elise Easter Christiansen <eec@dips.no>
2022-04-05 09:03:07 +0200

    frist commit

---
+++ src/Index.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Compositions from "./Compositions.svelte";
+  import { openEHR } from "./OpenehrStore";
+</script>
+
+{#if $openEHR}
+  Welcome to OpenEHR!
+  <Compositions />
+{:else}
+  Loading client...
+{/if}
